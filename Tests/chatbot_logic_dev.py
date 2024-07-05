8f9e6ddb81597a3f997b97d14250d19461d7d86d
Gerardo Ruiz <grville93@gmail.com>
2024-07-05 16:43:04 -0500

    Chatbot logic started testing in CLI. Needs more reviews and conversations paths before testing in Whatsapp

--- Tests/chatbot_logic_dev.py
+++ Tests/chatbot_logic_dev.py
@@ -23,6 +23,9 @@ logger = logging.getLogger(__name__)
 
 
 
+
+
+# This class will be the one who manage the patient's info storing, generating appointment variables and functions related with the chat
 class MessageHandler:
 
     # Variables Pools
@@ -75,34 +78,47 @@ class MessageHandler:
 
         except Exception as e:
             logger.error(f"Error sending message to {self.user_number}: {e}")
-    
 
+        
+    
 
     def check_reply(self, messange:str, reply_options:list[str]) -> str:
 
-        '''
-        This function validates that the user's response are within expected.
-        '''
+        '''This function validates that the user's response are within expected.'''
 
         while True:
-
-            # Send the greeting message to the user
-            self.send_message(messange)
-        
-            # Need to figure how to capture the response from the server
-            user_response = ''' Something to take the User's response from the server '''
             
+            '# ORIGINAL VERSION'
+            # # Send the greeting message to the user
+            # self.send_message(messange)
+
+            '# CLI VERSION'
+            user_response = input(messange)
+        
+
+            # # Need to figure how to capture the response from the server
+            # user_response = ''' Something to take the User's response from the server '''
+            
+
             # Check for early exit
             if user_response.lower() == 'exit':
                 return self.early_exit() # Early exit the conversation
             
+
             # Check if the user's response is within expected
             if user_response.lower() in reply_options:
-                return user_response
+                return user_response.lower()
             
-            # Non-complying user's reply
+            
+            # # Non-complying user's reply
             non_complying_reply = f'Sorry! you reply is not one of the available options.'
-            self.send_message(non_complying_reply)
+
+            '# ORIGINAL VERSION'
+            # self.send_message(non_complying_reply)
+            
+            
+            '# CLI VERSION'
+            print(non_complying_reply)
 
 
             
@@ -113,8 +129,20 @@ class MessageHandler:
 
             - This must include an early closing message sent to the customer and the DB closing.           
         '''
-                
-        pass
+        
+        early_closing_message = f'''Alright! Thank you for reaching out St. John's Health Group Virtual Assistance Service. ✝️🧑‍⚕️
+
+We will close this query.
+
+Thanks for preferring our services! 😊​
+We hope you get better in no time ❤️‍🩹
+'''
+        
+        '# ORIGINAL VERSION'
+        # PENDING...
+        
+        '# CLI VERSION'
+        return print(early_closing_message)
 
 
 
@@ -151,16 +179,26 @@ I'm here to help you with any questions or issues you might have regarding your 
 Before we move on, could you please provide your ID followed by your full name so I can address your request appropiately?
 
 Please reply as follows: *ID's Number*, *Patient's Full Name*
+
 '''
     
-    #   Send the greeting message to the user
-    chatbot_session.send_message(greeting_message)
+    '# ORIGINAL VERSION'
+    # #   Send the greeting message to the user
+    # chatbot_session.send_message(greeting_message)
 
-    #   Need to figure how to capture the response from the server
-    user_response = ''' Something to take the User's response from the server '''
-    
+    # #   Need to figure how to capture the response from the server
+    # user_response = '''Something to take the User's response from the server '''
+
+
+    '# CLI VERSION'
+    user_response = input(greeting_message)
+
     #   Process the patient response to the greeting
     chatbot_session.patient_id, chatbot_session.patient_name = [elem.strip().title() for elem in user_response.split(',')]
+
+
+
+    '''**A check of the first message is required!**'''
 
 
 
@@ -169,7 +207,7 @@ Please reply as follows: *ID's Number*, *Patient's Full Name*
     #   Confirm with a message using patient's name, instruct for the following step and capture the patient's discomfort 
     new_message = f'''Alright, {chatbot_session.patient_name}, thanks for reaching out. 😊​
 
-⚠️​Note: You can end this chat any time by replying with 'exit' and the query will be closed. 
+⚠️​Note: You can end this chat any time by replying with 'exit' and your query will be closed. 
 
 Now, what is the reason for your query?
 
@@ -178,8 +216,10 @@ Now, what is the reason for your query?
     - Gastrointestinal Issues
     - Joint/Muscular Discomfort
     - Others
-'''    
 
+'''    
+    
+    # Check the user's reply is within expected
     user_response = chatbot_session.check_reply(new_message, ['general discomfort', 'respiratory difficulties', 'gastrointestinal issues', 'joint/muscular discomfort', 'others'])
     
     #   Save the Patient Discomfort
@@ -192,8 +232,11 @@ Now, what is the reason for your query?
     #   Check if the patient has a pre-existing diagnosis for their discomfort 
     new_message = f'''Do you have a previous diagnosis for your current ailment?
 - Yes
-- No'''        
+- No
+
+'''        
    
+    # Check the user's reply is within expected
     user_response = chatbot_session.check_reply(new_message, ['yes', 'no'])
 
     # Generating a pre-existing ailment
@@ -202,9 +245,10 @@ Now, what is the reason for your query?
         # pre_existences_pool dict Class var, in 'patient_discomfort' instance var. 
         pre_existing_ailments = chatbot_session.pre_existences_pool[chatbot_session.patient_discomfort]
 
-        ailment_index = randint(0, len(pre_existing_ailments))  # Generate a random index to have a pre-existing ailment
+        # Generate a random index to have a pre-existing ailment
+        ailment_index = randint(0, len(pre_existing_ailments)-1)  
 
-        # Save the random generated patient pre-existence
+        # Save the randomly generated patient pre-existence
         chatbot_session.patient_pre_existence = pre_existing_ailments[ailment_index]
 
 
@@ -214,8 +258,11 @@ Now, what is the reason for your query?
     #   Check if the patient want to be treated by their "current treating doctor"
     new_message = f'''Would you like to be attended by your current treating doctor?
 - Yes
-- No'''    
+- No
 
+'''    
+
+    # Check the user's reply is within expected
     user_response = chatbot_session.check_reply(new_message, ['yes', 'no'])
 
     # Generating a dr's name and last name
@@ -226,11 +273,11 @@ Now, what is the reason for your query?
         dr_last_names_options = chatbot_session.drs_names_and_last_names_pool['last_names']
 
         # Generate the seeds of the resulting dr's name and last name
-        dr_names_index = randint(0, len(dr_names_options))
-        dr_last_name_index = randint(0, len(dr_last_names_options))
+        dr_names_index = randint(0, len(dr_names_options)-1)
+        dr_last_name_index = randint(0, len(dr_last_names_options)-1)
 
-        # Set the actual dr's name and last name
-        chatbot_session.treating_dr = f'{dr_names_options[dr_names_index]} {dr_last_names_options[dr_last_name_index]}'
+        # Define the actual dr's name and last name
+        user_response = f'{dr_names_options[dr_names_index]} {dr_last_names_options[dr_last_name_index]}'
     
 
     else:
@@ -243,30 +290,45 @@ Now, what is the reason for your query?
         dr_last_names_options = chatbot_session.drs_names_and_last_names_pool['last_names']
 
         # Generate seeds to randomly generate 3 different options for drs names and last names
-        name_op1, name_op2, name_op3 = sample(range(dr_names_options),3)
-        lastname_op1, lastname_op2, lastname_op3 = sample(range(dr_last_names_options),3)
+        name_op1, name_op2, name_op3 = sample(dr_names_options,3)
+        lastname_op1, lastname_op2, lastname_op3 = sample(dr_last_names_options,3)
 
         # Define the actual 3 options of drs for the user to choose
-        dr_op_1 = f'{dr_names_options[name_op1]} {dr_last_names_options[lastname_op1]}'
-        dr_op_2 = f'{dr_names_options[name_op2]} {dr_last_names_options[lastname_op2]}'
-        dr_op_3 = f'{dr_names_options[name_op3]} {dr_last_names_options[lastname_op3]}'
+        dr_op_1 = f'{name_op1} {lastname_op1}'
+        dr_op_2 = f'{name_op2} {lastname_op2}'
+        dr_op_3 = f'{name_op3} {lastname_op3}'
 
         # Generate the text body that will be sent to the user
+        #   the 'dr_speciality[:-1]' is to name the singular of the dr specialities
         new_message = f'''Ok! Currently we count with this {dr_speciality} to take care of you ailment:
 1. {dr_op_1}
 2. {dr_op_2}
 3. {dr_op_3}
 
 Which {dr_speciality[:-1]} would you like be attended by?
-''' # the 'dr_speciality[:-1]' is to name the singular of the dr specialities
+
+'''
         
-    # Create the list of possible replies
-    users_replies = [dr_op_1.lower(), dr_op_2.lower(), dr_op_3.lower(), '1', '2', '3']
+        # Create the list of possible replies
+        user_options = [dr_op_1.lower(), dr_op_2.lower(), dr_op_3.lower(), '1', '2', '3']
 
-    # Check the user's reply is within expected
-    user_response = chatbot_session.check_reply(new_message, users_replies)
+        # Check the user's reply is within expected
+        user_response = chatbot_session.check_reply(new_message, user_options)
 
-    # Save the user's choosen dr
+
+    # Convert the user's choice into the actual dr's full name
+    if user_response in '123':
+
+        if user_response == '1':
+            user_response = dr_op_1
+        
+        elif user_response == '2':
+            user_response = dr_op_2
+        
+        else:
+            user_response = dr_op_3
+    
+    # Save the user's choosen doctor
     chatbot_session.treating_dr = user_response.title()
 
 
@@ -277,26 +339,31 @@ Which {dr_speciality[:-1]} would you like be attended by?
     new_message = f'''Would you rather having the appointment virtuall or presentially?
 
 - Presential
-- Virtual'''    
+- Virtual
 
-    # Check the user's reply
+'''    
+
+    # Check the user's reply is within expected
     user_response = chatbot_session.check_reply(new_message, ['presential', 'virutal'])
 
-    # Determine patient's ailment (Gral or specific, if there is a pre-existence)
+    # Save patient's appointment type
+    chatbot_session.appointment_type = user_response
+
+    # Define patient's ailment (Gral or specific, if there is a pre-existence)
     patient_ailment = chatbot_session.patient_pre_existence if chatbot_session.patient_pre_existence else chatbot_session.patient_discomfort
 
     # Configure the closing message according to the appointment type selected
-    if user_response.lower() == 'presential':
+    if chatbot_session.appointment_type == 'presential':
 
-        # Determine the day, the time and location of the appointment randomly
+        # Define the day, the time and location of the appointment randomly
         #   Seeds
-        time_index = randint(0,len(chatbot_session.time_pool))
-        weekday_index = randint(0,len(chatbot_session.weekday_pool))
-        med_center_index = randint(0,len(chatbot_session.med_cent_pool))
+        time_index = randint(0,len(chatbot_session.time_pool)-1)
+        weekday_index = randint(0,len(chatbot_session.weekday_pool)-1)
+        med_center_index = randint(0,len(chatbot_session.med_cent_pool)-1)
 
         #   Actual time, weekday and location
         appointment_time = chatbot_session.time_pool[time_index]
-        appointment_weekday = chatbot_session.time_pool[weekday_index]
+        appointment_weekday = chatbot_session.weekday_pool[weekday_index]
         appointment_med_center = chatbot_session.med_cent_pool[med_center_index]
 
         # Generate the closing message
@@ -308,7 +375,9 @@ in {appointment_med_center} the next {appointment_weekday} at {appointment_time}
 Please remember to be present at reception 15 minutes prior to your appointment with your valid ID.
 
 Thanks for preferring our services! 😊​
-We hope you get better in no time ❤️‍🩹​'''
+We hope you get better in no time ❤️‍🩹
+
+​'''
 
 
     else:
@@ -322,12 +391,18 @@ virtually to review patient's {patient_ailment.title()}.
 Please remember be online 10 minutes prior to the appointment and also be sure to have a stable connection, access to a webcam and microphone to make sure the appointment will happen without inconvenience.
 
 Thanks for preferring our services! 😊​
-We hope you get better in no time ❤️‍🩹​'''
+We hope you get better in no time ❤️‍🩹
+
+​'''
 
 
-    # Send the message to the user
-    chatbot_session.send_message(new_message)
+    '# ORIGINAL VERSION'
+    # # Send the message to the user
+    # chatbot_session.send_message(new_message)
 
+
+    '# CLI VERSION'
+    print(new_message)
 
     # Close the DB session 
     #Write the code to close the DB session
@@ -341,3 +416,4 @@ We hope you get better in no time ❤️‍🩹​'''
 
 
 
+conversation('3205503934')
